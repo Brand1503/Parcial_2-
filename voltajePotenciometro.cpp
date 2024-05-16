ddb863f65d62b6dc815f6f7e770ce4c54021a434
valentina-mera <134455726+valentina-mera@users.noreply.github.com>
2024-05-16 14:21:19 -0500

    Update voltajePotenciometro.cpp
    cambio de delay a millis

--- voltajePotenciometro.cpp
+++ voltajePotenciometro.cpp
@@ -1,16 +1,24 @@
 #include <Arduino.h>
-#define pot1 39
+#define pot1 39 
+
 int valor;
 float Voltaje=0;
+unsigned long acMillis = 0;
+const long prevmillis = 1000;
 void setup() {
   Serial.begin(9600);
  
 }
 
-void loop() {
-  valor=analogRead(pot1);
+void loop() { 
+  unsigned long millisactual = millis();
+  if (millisactual-prevmillis>=prevmillis)
+  {
+    acMillis=millisactual;
+valor=analogRead(pot1);
   Voltaje=(3.3/4095.0)*valor;
    Serial.print("\nVoltaje: \n" );
   Serial.print(Voltaje);
-  delay(1000);
+  }
+  
 }
